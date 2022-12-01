ebc7ef4a7bc9fd0caa076be078766dc9c4e2e16e
Istiak Ahmed <istiakahmed104127@gmail.com>
2022-12-01 19:30:38 +0600

    redux setup

---
+++ src/utlis/filterData.js
@@ -0,0 +1,34 @@
+export const launchDate = [
+     {
+       name: "Last Week",
+       day: 7,
+     },
+     {
+       name: "Last Month",
+       day: 30,
+     },
+     {
+       name: "Last Year",
+       day: 365,
+     },
+   ];
+   export const launchStatus = [
+     {
+       name: "Success",
+       status: true,
+     },
+     {
+       name: "Failure",
+       status: false,
+     },
+   ];
+   export const launchUpcoming = [
+     {
+       name: " True",
+       status: true,
+     },
+     {
+       name: "False",
+       status: false,
+     },
+   ];
